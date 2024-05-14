24254e50b7e549310da7a309358ff92a6dd73691
Muhammad Aiman Zikri Zega <zikrizega@upi.edu>
2024-05-14 19:43:25 +0700

    up

--- snake.py
+++ snake.py
@@ -14,6 +14,9 @@ size_y = 360
 pygame.display.set_caption('Ular makan')
 screen = pygame.display.set_mode((size_x, size_y))
 
+#snake
+snake_body = [100,50]
+
 #======= COLORS =======
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
@@ -28,4 +31,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    #update to screen white again
+    screen.fill(white)
+    #create snake
+    pygame.draw.ellipse(screen, black, pygame.Rect(snake_body[0], snake_body[1], 10, 10))
+    #snake run
+    snake_body[0] += 10
+    #level
+    pygame.time.Clock().tick(10)
+    #update screen
     pygame.display.update()
