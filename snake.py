b883ede0970a854a44ef70eef2013df1a55bf20f
Muhammad Aiman Zikri Zega <zikrizega@upi.edu>
2024-05-14 16:59:48 +0700

    update

--- snake.py
+++ snake.py
@@ -8,10 +8,24 @@ else:
 
 #======= WINDOWS GAME =======
 size_x = 720
-size_y = 480
+size_y = 360
 
+#title 
 pygame.display.set_caption('Ular makan')
 screen = pygame.display.set_mode((size_x, size_y))
 
+#======= COLORS =======
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+green = pygame.Color(0, 255, 0)
+
+#background color
+screen.fill(white)
+
+#run program
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
     pygame.display.update()
